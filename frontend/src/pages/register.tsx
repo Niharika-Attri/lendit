6e4a2dbaf2e28e9a508ddf98dc42948d8fa3ec6b
Niharika_Attri <niharikaattri2005@gmail.com>
2025-07-14 09:18:15 +0530

    vite setup

---
+++ frontend/src/pages/register.tsx
@@ -0,0 +1,11 @@
+function Register(){
+    return(
+        <div className="flex justify-center items-center w-screen h-screen ">
+            <div className=" w-80 h-96">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Register
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react(),nodePolyfills({
+      protocolImports: true,
+    }),],
+    server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
